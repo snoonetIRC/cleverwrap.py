104f50edf5dc32f1ee79e941fa1546962d8e980f
linuxdaemon <linuxdaemon.irc@gmail.com>
2019-08-26 09:35:17 -0500

    Revert "Rename package to cleverwrap2"
    This reverts commit 11c37fec

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-from setuptools import setup
-
+from distutils.core import setup
 setup(
-    name='cleverwrap2',
-    packages=['cleverwrap'],
-    license='MIT',
-    install_requires=['requests'],
-    version='0.1.0',
-    url='https://github.com/snoonetIRC/cleverwrap2',
-    description='A wrapper for the official cleverbot.com API',
-    author='linuxdemon',
-    author_email='linuxdaemon@snoonet.org',
-    keywords=['cleverbot', 'wrapper', 'clever'],
-    classifiers=[
-        'Programming Language :: Python :: 3 :: Only',
-        'License :: OSI Approved :: MIT License',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-    ],
+          name = 'cleverwrap',
+          packages = ['cleverwrap'],
+          license = 'MIT',
+          install_requires = ['requests'],
+          version = '0.2.3.2',
+          description = 'A wrapper for the official cleverbot.com API',
+          author = 'Andrew Edwards',
+          author_email = 'andrewthomasedwards@gmail.com',
+          url = 'https://github.com/edwardslabs/cleverwrap.py',
+          download_url = 'https://github.com/edwardslabs/cleverwrap.py/tarball/0.2.3.2',
+          keywords = ['cleverbot', 'wrapper', 'clever'],
+          classifiers =[
+              'Programming Language :: Python :: 3 :: Only',
+              'License :: OSI Approved :: MIT License',
+              'Intended Audience :: Developers',
+              'Natural Language :: English',
+          ],
 )
